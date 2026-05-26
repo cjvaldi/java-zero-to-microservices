5ad6cda8e875a0afe646b40789cf067854030a0a
CRISTIAN VALDIVIESO VALENZUELA <cjyape@hotmail.com>
2026-05-26 23:21:37 +0200

    Refactor: Integrar manejo de excepciones en el Repositorio Genérico
    - Implementación de validaciones con excepciones robustas para las operaciones CRUD.
    
    - Lanzamiento de errores controlados en caso de registros duplicados o elementos no encontrados.

---
+++ 12_poo_interfases_repositorio/src/org/cjvaldi/poointerfases/excepciones/AccesoDatoException.java
@@ -0,0 +1,7 @@
+package org.cjvaldi.poointerfases.excepciones;
+
+public class AccesoDatoException extends Exception{
+    public AccesoDatoException(String message) {
+        super(message);
+    }
+}
---
+++ 12_poo_interfases_repositorio/src/org/cjvaldi/poointerfases/excepciones/EscrituraAccesoDatoException.java
@@ -0,0 +1,7 @@
+package org.cjvaldi.poointerfases.excepciones;
+
+public class EscrituraAccesoDatoException extends AccesoDatoException {
+    public EscrituraAccesoDatoException(String message) {
+        super(message);
+    }
+}
