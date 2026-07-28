0b65b90d63cd834b27ba9da8593d2a7a612109bb
CRISTIAN VALDIVIESO VALENZUELA <cjyape@hotmail.com>
2026-07-29 01:03:50 +0200

    Feat: Agregar Sección 91 - Spring Cloud: Comenzando con el Microservicio Products
    - Inicialización del proyecto msvc-products y modelado de entidades de dominio JPA.
    
    - Implementación de la capa de acceso a datos mediante repositorios Spring Data JPA y lógica de negocio en el Service.
    
    - Exposición de endpoints API REST en el Controller y configuración de persistencia en MySQL via application.properties.
    
    - Carga de scripts SQL iniciales, verificación de tablas en BBDD y pruebas de integración con Postman.

---
+++ 10-springCloud/msvc-products/src/main/java/com/cjvaldi/springcloud/msvc/products/controllers/ProductController.java
@@ -0,0 +1,40 @@
+package com.cjvaldi.springcloud.msvc.products.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cjvaldi.springcloud.msvc.products.entities.Product;
+import com.cjvaldi.springcloud.msvc.products.services.ProductService;
+
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+// @RequestMapping("/api/products")  // para ver la raiz
+public class ProductController {
+
+    final private ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> list(){
+        return ResponseEntity.ok(this.service.findAll());
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity <Product> details(@PathVariable Long id) {
+        Optional<Product> productOptional =  service.findById(id);
+        if(productOptional.isPresent()){
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+    
+
+}
