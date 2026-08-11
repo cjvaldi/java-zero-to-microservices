db691100b1e4b3a2dab9c0bbdb08666bfba7bf50
CRISTIAN VALDIVIESO VALENZUELA <cjyape@hotmail.com>
2026-08-11 23:31:30 +0200

    Feat: Agregar Sección 100 - Spring Boot: Interceptores HTTP
    - Introducción e implementación de interceptores HandlerInterceptor (preHandle, postHandle y afterCompletion) para interceptar peticiones HTTP.
    
    - Caso de uso 1 (13-spring-boot-form): Medición del tiempo transcurrido de ejecución en peticiones HTTP y registro en MvcConfig (WebMvcConfigurer).
    
    - Caso de uso 2 (14-spring-boot-horario): Desarrollo del proyecto para control e interceptación de horario de acceso a clientes y gestión de respuestas fuera de horario.

---
+++ 13-spring-boot-form/src/main/java/com/cjvaldi/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -0,0 +1,68 @@
+package com.cjvaldi.springboot.form.app.interceptors;
+
+import java.util.Random;
+
+import org.jspecify.annotations.Nullable;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component("tiempoTranscurridoInterceptor")
+public class TiempoTranscurridoInterceptor implements HandlerInterceptor{
+	
+	private static final Logger logger = org.slf4j.LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		if(request.getMethod().equalsIgnoreCase("post")) {
+			return true;
+		}
+		
+		if(handler instanceof HandlerMethod) {
+			HandlerMethod metodo = (HandlerMethod) handler;
+			logger.info("es un método del controlador: " + metodo.getMethod().getName());
+		}
+		logger.info("TiempoTranscurridoInterceptor: preHandle() entrando...");
+		logger.info("Interceptando: "+handler);
+		long tiempoInicio = System.currentTimeMillis();
+		request.setAttribute("tiempoInicio",tiempoInicio);
+		
+		// simulamos una carga, un delay
+		Random random = new Random();
+		Integer demora = random.nextInt(100);
+		Thread.sleep(demora);
+		//response.sendRedirect(request.getContextPath().concat("/login")); // para rediriguir a otra pagina,
+		// solo como ejemplo, no es práctico.
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			@Nullable ModelAndView modelAndView) throws Exception {
+		
+		if(request.getMethod().equalsIgnoreCase("post")) {
+			return;
+		}
+		
+		long tiempoFin = System.currentTimeMillis();
+		long tiempoInicio= (Long) request.getAttribute("tiempoInicio");
+		long tiempoTranscurrido = tiempoFin - tiempoInicio;
+		
+		if(handler instanceof HandlerMethod && modelAndView != null) {
+			modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
+		}
+		
+		logger.info("Tiempo Trancurrido: "+ tiempoTranscurrido+ "  milisegundos");
+		logger.info("TiempoTranscurridoInterceptor: postHandle() saliendo...");
+	}
+	
+	
+
+}
